a5ad7c54ac5aadc16fdfb2f0ade5bdef6465ecc1
amitkumar2-git <68748781+amitkumar2-git@users.noreply.github.com>
2020-11-30 23:32:35 +0530

    Initial app architechture

---
+++ LaunchPad.Mobile/LaunchPad.Mobile/Views/SplashPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LaunchPad.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Delay(3000);
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
